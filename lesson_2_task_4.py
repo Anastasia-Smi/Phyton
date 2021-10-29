d91eb90a55fb6543c1b0c5c97b4b52ca02ab2a01
Anastasia Smirnov <a.kurdeka@gmail.com>
2021-10-28 21:12:56 -0400

    lesson_2_task_1.py

---
+++ lesson_2_task_4.py
@@ -0,0 +1,7 @@
+names = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
+names_2 = list(map(str.upper, names))
+message = 'HELLO '
+print(message + (names_2[0]))
+print(message + (names_2[1]))
+print(message + (names_2[2]))
+print(message + (names_2[3]))
